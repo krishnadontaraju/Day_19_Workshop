5a381dc2b297f4d3acee5c88054e3c811f5c8858
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-04-15 10:00:05 +0530

    MODIFIED : to find cheapest hotel

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,8 +1,65 @@
 public class Hotel {
-    String hotelName;
-    int rating;
-    int weekdayPrice;
-    int weekendPrice;
-    int rewardWeekdayPrice;
-    int getRewardWeekendPrice;
+    public String hotelName;
+    public int price;
+    public int rating;
+    public int weekdayPrice;
+    public int weekendPrice;
+    public int rewardWeekdayPrice;
+    public int getRewardWeekendPrice;
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getWeekdayPrice() {
+        return weekdayPrice;
+    }
+
+    public void setWeekdayPrice(int weekdayPrice) {
+        this.weekdayPrice = weekdayPrice;
+    }
+
+    public int getWeekendPrice() {
+        return weekendPrice;
+    }
+
+    public void setWeekendPrice(int weekendPrice) {
+        this.weekendPrice = weekendPrice;
+    }
+
+    public int getRewardWeekdayPrice() {
+        return rewardWeekdayPrice;
+    }
+
+    public void setRewardWeekdayPrice(int rewardWeekdayPrice) {
+        this.rewardWeekdayPrice = rewardWeekdayPrice;
+    }
+
+    public int getGetRewardWeekendPrice() {
+        return getRewardWeekendPrice;
+    }
+
+    public void setGetRewardWeekendPrice(int getRewardWeekendPrice) {
+        this.getRewardWeekendPrice = getRewardWeekendPrice;
+    }
 }
--- src/main/java/HotelReservationSystem.java
+++ src/main/java/HotelReservationSystem.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class HotelReservationSystem {
+    Scanner fetch =  new Scanner(System.in);
+
     Hotel lakeWood = new Hotel();
     Hotel bridgeWood = new Hotel();
     Hotel ridgeWood = new Hotel();
+
+
+    public void findCheapestWithTwoDays(){
+        lakeWood.setHotelName("LAKE WOOD");
+        lakeWood.setPrice(220);
+
+        System.out.println("WELCOME TO THE HOTEL RESERVATION");
+        System.out.println("WHICH DAY DO YOU WANT TO BOOK ?" +
+                            "1.10 September 2020 \n2.11 September 2020");
+
+        switch (fetch.nextInt()){
+            case 1:
+                System.out.println("THE CHEAPEST HOTEL IS "+lakeWood.getHotelName()+"WHICH IS ONLY $"+lakeWood.getPrice());
+                break;
+            case 2:
+                System.out.println("THE CHEAPEST HOTEL IS "+lakeWood.getHotelName()+"WHICH IS ONLY $"+lakeWood.getPrice());
+                break;
+            default:
+                System.out.println("NOT CORRECT INPUT");
+                break;
+        }
+    }
 }
