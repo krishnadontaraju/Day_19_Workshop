68ec7a81d63bb6fc3b92899cb2a9b910df9efb49
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-04-15 21:27:32 +0530

    ADDED : test to validate the cheapest

---
+++ src/test/java/HotelReservationTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class HotelReservationTest {
+    @Test
+    public void givenDateRange_whenAvailable_shouldReturnHotels(){
+
+        HotelReservationSystem newSystem = new HotelReservationSystem();
+        newSystem.checkInDate = LocalDate.parse("2020-09-11");
+        newSystem.checkOutDate = LocalDate.parse("2020-09-12");
+        try {
+            newSystem.findCheapestHotelWithInGivenTimeline();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        Assertions.assertEquals(newSystem.cheapestHotelList.size(),2);
+    }
+}
